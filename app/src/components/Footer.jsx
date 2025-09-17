51cf9dbad7bdfa5e38a2930ecfd945c3fdceadd3
zubia-d <dahrizubia222@gmail.com>
2025-09-17 20:51:14 +0500

    new commit

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -1,10 +1,22 @@
+import { CiLinkedin } from "react-icons/ci"
+import { CiInstagram } from "react-icons/ci";
 
 const Footer = () => {
   return (
-    <div className=" bg-rose-300 w-full">
-        <h2 className="text-center">All rights reserved @shop_minoa</h2>
+ <div>
+      <div className="bg-rose-200">
+          <div className="flex p-3 gap-2 justify-center">
+            <a href="https://www.instagram.com/zubiaaaaa_a/" target="blank">
+            
+           <CiInstagram size={24} /> 
+            </a>
+            <p>shop_minoa</p>
+          </div>    
+      </div>
+      <h3 className="text-center bg-rose-400 text-white">
+        All rights reserved @zubia-ghulamullah{" "}
+      </h3>
     </div>
-  )
-}
-
+  );
+};
 export default Footer;
